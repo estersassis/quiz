f308b7a4eb281d67e151f5cb9aaf2c18b2a31106
Ester Sara Assis Silva <ester.assis@prosperita.io>
2025-09-05 18:19:11 -0300

    Commit 2: Creating 10 unit tests

--- tests.py
+++ tests.py
@@ -25,6 +25,12 @@ def test_create_question_with_valid_points():
     question = Question(title='q1', points=100)
     assert question.points == 100
 
+def test_create_question_with_invalid_points(): #
+    with pytest.raises(Exception):
+        Question(title='q1', points=0)
+    with pytest.raises(Exception):
+        Question(title='q1', points=101)
+
 def test_create_choice():
     question = Question(title='q1')
     
@@ -34,3 +40,78 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+def test_create_multiple_choices():
+    question = Question(title='q1')
+    
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+
+    assert len(question.choices) == 2
+    assert question.choices[0].id != question.choices[1].id
+
+def test_create_choice_with_invalid_text():
+    question = Question(title='q1') 
+    
+    with pytest.raises(Exception):
+        question.add_choice('', False)
+    with pytest.raises(Exception):
+        question.add_choice('a'*101, False)
+
+def test_remove_choice_by_id():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+
+    question.remove_choice_by_id(1)
+
+    assert len(question.choices) == 0
+
+def test_remove_choice_by_id_with_invalid_id():
+    question = Question(title='q1')
+
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(1)
+
+def test_remove_choice_all_choices():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+
+    question.remove_all_choices()
+
+    assert len(question.choices) == 0
+
+def test_set_correct_choices():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+
+    question.set_correct_choices([1])
+
+    assert question.choices[0].is_correct == True
+    assert question.choices[1].is_correct == False
+
+def test_set_correct_choices_with_invalid_id():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+
+    with pytest.raises(Exception):
+        question.set_correct_choices([2])
+
+def test_correct_selected_choices():
+    question = Question(title='q1', max_selections=2)
+    question.add_choice('a', False)
+    question.add_choice('b', True)
+    
+    corrected_choices = question.correct_selected_choices([1, 2])
+
+    assert len(corrected_choices) == 1
+    assert corrected_choices == [2]
+
+def test_correct_selected_choices_with_more_than_max_selections():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+    question.add_choice('b', True)
+    
+    with pytest.raises(Exception):
+        _ = question.correct_selected_choices([1, 2])
